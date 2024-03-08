f003e96b0c17d81e39d0632b29bb7850e6ab6c70
Flávia lemes <105284601+Flavialemes@users.noreply.github.com>
2024-03-08 09:22:01 -0300

    ok

---
+++ main.js
@@ -0,0 +1,13 @@
+const botoes = document.querySelectorAll(".botao")
+
+for (Let i = 0; i < botoes.length; i++); {
+    botoes[i].onclick = function () {
+
+        for (Let j = 0; j < botoes.length; j++); {
+            botoes[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+    }
+}
+
